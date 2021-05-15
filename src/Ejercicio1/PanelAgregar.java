dcfbce3412b29ed419dcb3e33e31d3a7c433cf66
brandan.fernando <febrandan@indracompany.com>
2021-05-14 22:32:32 -0300

    Se agrega elementos para agregar -> punto A/B

--- src/Ejercicio1/PanelAgregar.java
+++ src/Ejercicio1/PanelAgregar.java
@@ -2,19 +2,69 @@ package Ejercicio1;
  
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelAgregar extends JPanel {
-  
+	
+	private JTextField tbNuevaPelicula;
+	private JComboBox<Genero> cbGenero;
+	private JButton btnAceptar;
+	
 	public PanelAgregar() {
 		setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 430, 278);
+		panel.setBounds(10, 11, 450, 250);
 		add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblPanelAgregar = new JLabel("Panel agregar");
-		panel.add(lblPanelAgregar);
-
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(92, 16, 70, 40);
+		panel.add(lblId);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(92, 62, 70, 40);
+		panel.add(lblNombre);
+		
+		JLabel lblGenero = new JLabel("Genero");
+		lblGenero.setBounds(92, 113, 70, 40);
+		panel.add(lblGenero);
+		
+		JLabel lbID = new JLabel("IDitem");
+		lbID.setBounds(172, 11, 70, 50);
+		panel.add(lbID);
+		
+		lbID.setText(String.valueOf(Peliculas.ProximoId()));
+		
+		tbNuevaPelicula = new JTextField();
+		tbNuevaPelicula.setBounds(172, 72, 163, 20);
+		panel.add(tbNuevaPelicula);
+		tbNuevaPelicula.setColumns(10);
+		
+		cbGenero = new JComboBox<Genero>();
+		cbGenero.setBounds(172, 123, 163, 20);
+		panel.add(cbGenero); 
+		
+		//cbGenero.addItem("Seleccione un genero");
+		cbGenero.addItem(new Genero("Terror"));
+		cbGenero.addItem(new Genero("Accion"));
+		cbGenero.addItem(new Genero("Suspenso"));
+		cbGenero.addItem(new Genero("Romantica"));
+		
+		btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) { 
+				
+				Peliculas nuevaPelicula = new Peliculas(tbNuevaPelicula.getText(), String.valueOf(cbGenero.getSelectedItem()));				
+				JOptionPane.showMessageDialog(null, nuevaPelicula.toString());				
+			}
+		});
+		btnAceptar.setBounds(92, 184, 89, 23);
+		panel.add(btnAceptar);
 	}
-
 }
--- src/Ejercicio1/Peliculas.java
+++ src/Ejercicio1/Peliculas.java
@@ -1,5 +1,52 @@
 package Ejercicio1;
 
 public class Peliculas {
+	
+	private int id;
+	private String nombre;
+	private String genero;
+	private static int cont = 0;
+	
+	public static int ProximoId() {
+		return cont+1;
+	}
+	
+	public Peliculas() {
+	}
+
+	public Peliculas(String nombre, String genero) {
+		cont ++;
+		this.id = cont;
+		this.nombre = nombre;
+		this.genero = genero;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	@Override
+	public String toString() {
+		return "Peliculas-> ID: " + this.id + " Nombre: " + this.nombre + " Genero: " + this.genero;
+	}
+	
+	
+	
 
 }
